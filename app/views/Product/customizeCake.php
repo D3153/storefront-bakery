86a67b04db7793f5e93a699f2d9975104e785d91
JiaminYuan <ymimi1121@gmail.com>
2022-11-07 11:13:26 -0500

    customize cake in progress

--- app/views/Product/customizeCake.php
+++ app/views/Product/customizeCake.php
@@ -11,6 +11,44 @@
 </head>
 
 <body>
+  <nav class="navbar navbar-expand-lg navbar-dark d-none d-lg-block" style="z-index: 2000;">
+        <div class="container-fluid">
+          <a class="navbar-brand nav-link"><strong>Pink Bakery</strong></a>
+            <div class="collapse navbar-collapse" id="navbarExample01">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                   <li class="nav-item">
+                      <a class="nav-link" href="/Product/shopAll" target="_blank">Shop All</a>
+                    </li>
+                      <div class="dropdown">
+                        <a class="nav-link" data-bs-toggle="dropdown">Shop by category</a>
+                        <div class="dropdown-menu">
+                          <a href="/Product/bread" class="dropdown-item">Bread</a>
+                          <a href="/Product/cookies" class="dropdown-item">Cookies</a>
+                          <a href="/Product/pies" class="dropdown-item">Pies</a>
+                          <a href="/Product/pastries" class="dropdown-item">Pastries</a>
+                          <a href="/Product/cakes" class="dropdown-item">Cakes</a>
+                          <a href="/Product/newArrivals" class="dropdown-item">New Arrivals</a>
+                      </div>
+                    </div>
+                    <li class="nav-item">
+                      <a class="nav-link" href="/Product/customizeCake" target="_blank">Customize Cake</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="/User/contactUs" target="_blank">Contact Us</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="/User/myAccount" target="_blank">My Account</a>
+                    </li>
+                    <li class="nav-item" style="float:right">
+                      <a class="nav-link" href="/User/cart" target="_blank" id="logout">Shopping Cart</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="/User/logout" target="_blank" id="logout">Logout</a>
+                    </li>
+              </ul>
+            </div>
+        </div>
+      </nav>
   <div class="mask d-flex align-items-center h-100 gradient-custom-3">
     <div class="container h-100">
       <div class="row d-flex justify-content-center align-items-center h-100">
