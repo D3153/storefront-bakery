25e3976c0734f9361df429ad574e72ae1a8354f2
D3153 <82058076+D3153@users.noreply.github.com>
2022-11-21 12:29:25 -0500

    cart model

--- app/models/Cart.php
+++ app/models/Cart.php
@@ -3,22 +3,40 @@ namespace app\models;
 
 class Cart extends \app\core\Model{
 
-	public function getById($cart_id){
-		$SQL = "SELECT * FROM cart WHERE cart_id=:cart_id";
+//for order history
+	public function getByUser($user_id){
+		$SQL = "SELECT * FROM cart WHERE user_id=:user_id";
 		$STMT = self::$_connection->prepare($SQL);
-		$STMT->execute(['cart_id'=>$this->cart_id]);
+		$STMT->execute(['user_id'=>$this->user_id]);
 		$STMT->setFetchMode(\PDO::FETCH_CLASS, 'app\models\Cart');
 		return $STMT->fetch();
 	}
 
 	public function insertIntoCart()
 	{
-		$SQL = "INSERT INTO cart(user_id, product_id, custom_cake_id, quantity, total_price) VALUES (:user_id, :product_id, :custom_cake_id, :quantity, :total_price)";
+		$SQL = "INSERT INTO cart(user_id, product_id, custom_cake_id, quantity, total_price, shipping_id, status) VALUES (:user_id, :product_id, :custom_cake_id, :quantity, :total_price, :shipping_id, :status)";
 		$STMT = self::$_connection->prepare($SQL);
 		$STMT->execute(['user_id'=>$this->user_id,
 						'product_id'=>$this->product_id,
 						'custom_cake_id'=>$this->custom_cake_id,
 						'quantity'=>$this->quantity,
-						'total_price'=>$this->total_price]);
+						'total_price'=>$this->total_price,
+						'status'=>'cart']);
+	}
+
+	public function updateOrderStatus(){
+		$SQL = "UPDATE cart SET status=:status WHERE user_id = :user_id AND status = :intitStatus";
+		$STMT = self::$_connection->prepare($SQL);
+		$STMT->execute(['status'=>'paid',
+						'user_id'=>$this->user_id,
+						'initStatus'=>'cart']);
+	}
+
+	public function updateItemStatus(){
+		$SQL = "UPDATE cart SET status=:status WHERE product_id = :product_id AND status = :intitStatus";
+		$STMT = self::$_connection->prepare($SQL);
+		$STMT->execute(['status'=>$this->status,
+						'product_id'=>$this->product_id,
+						'initStatus'=>$this->initStatus]);
 	}
 }
