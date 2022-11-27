41dda069ac51959a612b99d8d562fabc020defaf
JiaminYuan <ymimi1121@gmail.com>
2022-11-27 15:17:57 -0500

    sent email in progress

--- app/controllers/Order.php
+++ app/controllers/Order.php
@@ -13,13 +13,6 @@ class Order extends \app\core\Controller{
 		if(isset($_POST['action'])){
 			header('location:/Product/shopAll');
 		}
-		$msg = "First line of text\nSecond line of text";
-
-		// use wordwrap() if lines are longer than 70 characters
-		$msg = wordwrap($msg,70);
-
-		// send email
-		mail("ymimi1121@gmail.com","My subject",$msg);
 		$this->view('Order/confirmation');
 
 	}
