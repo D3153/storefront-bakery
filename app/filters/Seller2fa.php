1b9b237f21f4f52ddfb067c5dbc17a6f0d798277
D3153 <82058076+D3153@users.noreply.github.com>
2022-12-01 11:16:55 -0500

    authentication done

--- app/filters/Seller2fa.php
+++ app/filters/Seller2fa.php
@@ -5,9 +5,11 @@ namespace app\filters;
 class Seller2fa extends \app\core\AccessFilter{
 	public function execute(){
 		if($_SESSION['role'] == 'seller'){
-			$user->get($_SESSION['username']);
+			$user = new \app\models\Seller(); 	
+			$user = $user->getByUserId($_SESSION['user_id']);
 			if($user->secret_key == null){
 				header('location:/Seller/setup2fa');
+				return true;
 			}else return false;
 		}else{
 			return false;
--- app/models/Seller.php
+++ app/models/Seller.php
@@ -10,4 +10,12 @@ class Seller extends \app\core\Model{
 		$STMT->setFetchMode(\PDO::FETCH_CLASS, 'app\models\Seller');
 		return $STMT->fetch();
 	}
+
+	public function getByUserId($user_id){
+		$SQL = "SELECT * from user WHERE user_id=:user_id";
+		$STMT = self::$_connection->prepare($SQL);
+		$STMT->execute(['user_id'=>$user_id]);
+		$STMT->setFetchMode(\PDO::FETCH_CLASS, 'app\models\Seller');
+		return $STMT->fetch();
+	}
 }
